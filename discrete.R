1c7d7be9cfcce21d460ea8dfaa8ecf9126444ac1
umontano <u.montano.j@gmail.com>
2022-11-22 15:26:07 -0600

    vi edit

--- discrete.R
+++ discrete.R
@@ -1,10 +1,14 @@
+library(broom)
+library(purrr)
+library(tidyr)
+library(dplyr)
 
 #function REGRESSIONS significant values
-one_by_one_significant_predictors_lm  <- function(predictors, one_response, thresh=0.05)
+one_by_one_significant_predictors_lm  <- function(predictors, one_response, threshold_significance = 0.05, factor_flag = FALSE)
 {
-        models  <- map(predictors, ~  lm(one_response ~ as.factor(.x) ))
+        models  <- map(predictors, ~  if(factor_flag) lm(one_response ~ as.factor(.x) ) else lm(one_response ~ .x))
 coeffs  <- map(models, ~coef(summary(.x))[-1, c( "Pr(>|t|)")]  )
-sig  <- map(coeffs, ~ .x[.x <= thresh ]  )
+sig  <- map(coeffs, ~ .x[.x <= threshold_significance ]  )
 selector <-  map_lgl(sig, ~length(.x) > 0)
 models  <- models[selector]
 trim <-  sig[selector]
@@ -21,3 +25,5 @@ results_predictors_response_one_by_one <- results_predictors_response_one_by_one
 return(results_predictors_response_one_by_one)
 }
 
+data(iris)
+rrrr <- send_responses_to_predictors_lm(iris[, 1:2], iris[, 3:4], 0.10, FALSE)
