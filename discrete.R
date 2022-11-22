202baece11a19f9b72710000bdcbfc071346142c
umontano <u.montano.j@gmail.com>
2022-11-22 17:05:07 -0600

    vi edit

--- discrete.R
+++ discrete.R
@@ -19,9 +19,6 @@ one_by_one_significant_predictors_lm  <- function(one_response, predictors, thre
 send_responses_to_predictors_lm <- function(responses_dataset, predictors_dataset, threshold_significance = 0.05, factor_flag = FALSE)
 {
 results_predictors_response_one_by_one <- lapply(responses_dataset, one_by_one_significant_predictors_lm, predictors_dataset, threshold_significance = 0.05, factor_flag = TRUE)
-#names(results_predictors_response_one_by_one) <- names(responses_dataset)
-#selector <- sapply(responses_dataset, function(x) x > 0)
-#results_predictors_response_one_by_one <- results_predictors_response_one_by_one[selector]
 return(results_predictors_response_one_by_one)
 }
 
