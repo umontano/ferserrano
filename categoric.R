10654f4d46df548f373f28ad3c2cd355f53b6a65
umontano <u.montano.j@gmail.com>
2022-11-22 20:58:03 -0600

    vi edit

---
+++ categoric.R
@@ -0,0 +1,73 @@
+
+#function REGRESSIONS significant values
+one_by_one_significant_predictors_lm  <- function(one_response, predictors, threshold_significance = 0.05, categorical_flag = FALSE)
+{
+	models  <- map(predictors, ~ if(categorical_flag) lm(one_response ~ as.factor(.x) ) else lm(one_response ~ .x))
+	coeffs  <- map(models, ~ coef(summary(.x))[-1, c( "Pr(>|t|)")]  )
+	sig  <- map(coeffs, ~ .x[.x <= threshold_significance ]  )
+	selector <-  map_lgl(sig, ~length(.x) > 0)
+	models  <- models[selector]
+	trim <-  sig[selector]
+	return(trim)
+}
+
+
+send_responses_to_predictors_lm <- function(responses_dataset, predictors_dataset, threshold_significance = 0.05, categorical_flag = FALSE)
+{
+	results_predictors_response_one_by_one <- lapply(responses_dataset, one_by_one_significant_predictors_lm, predictors_dataset, threshold_significance = 0.05, categorical_flag = TRUE)
+	selector <- sapply(results_predictors_response_one_by_one, function(x) length(x)>0)
+	return(results_predictors_response_one_by_one[selector])
+}
+
+merged_categorical_and_torrance_totals <- function(columns_dataset, sign=0.05)
+{
+merged_dataset <- factors %>%
+	add_id_column_numero %>%
+	merge(add_id_column_numero(scales), by='numero') %>%
+	merge(add_id_column_numero(raven), by='numero') %>%
+	merge(add_id_column_numero(columns_dataset), by='numero') %>%
+	mutate(numero=as.numeric(numero)) %>%
+	select(!starts_with('X'))
+
+tor <- merged_dataset %>%
+    select(c(names(columns_dataset), -numero))
+torrest <- merged_dataset %>%
+    select(-numero) %>%
+    select(!names(columns_dataset))
+
+
+categorical_names <- c('escuela', 'grupo', 'sexo', 'edad', 'percentil', 'rango', 'dx')
+categorical_dataset <- merged_dataset %>%
+	select(categorical_names) %>%
+	mutate(across(where(is.numeric), as.factor))
+
+return(list(tor, categorical_dataset))
+}
+
+
+library('tidytext')
+library('widyr')
+library('ggplot2')
+library('dplyr')
+library('tidyr')
+library('tibble')
+library('psych')
+library(broom)
+library(purrr)
+library(tidyr)
+library(dplyr)
+
+#Attach the original dx variable to cleaned raven
+source('https://github.com/umontano/CBQ_comandos_SPSS_lab_ChyC/raw/main/CBQ_comandosSPSS_lab_CHyC.R')
+raven_csv_original <- read.csv(raven_url)
+raven$dx <- as.factor(raven_csv_original$dx)
+
+
+#generate list of responses_torrancetorals and predictors_categorical for lm
+tor_categorical <- merged_categorical_and_torrance_totals(torrance_totals)
+
+#Send list to lm_mapped function
+categorical_results <- send_responses_to_predictors_lm(tor_categorical[[1]], tor_categorical[[2]], 0.05, TRUE)
+
+print(categorical_results)
+
