318696209311dcf10762e0929e69f9200a8f4701
umontano <u.montano.j@gmail.com>
2022-11-26 15:24:08 -0600

    ggplot edit

--- discrete.R
+++ discrete.R
@@ -6,6 +6,7 @@
 #source('/a/ferserrano/categoric.R')
 source('https://github.com/umontano/ferserrano/raw/main/categoric.R')
 
+
 #using torrance partial variabels generate lm list
 partor_categorical <- merged_categorical_and_torrance_totals(torrance)
 
@@ -18,36 +19,60 @@ print(partialtor_results)
 
 
 
-mean.yield.data <- crop.data %>%
-  group_by(fertilizer, density) %>%
-  summarise(
-      yield = mean(yield)
-  )
+
+library(dplyr)
+data(mtcars)
+complete_dataset <-  mtcars
+
+response_column <- mtcars$mpg
+grouping_column1 <- 'vs'
+grouping_column2 <- 'am'
+two_way_anova_graph <- function(complete_dataset, response_column, grouping_column1, grouping_column2, threshold_significance = 0.05)
+{
+#Make a data frame with the group labels
+#Now we need to make an additional data frame so we can add these groupwise differences to our graph.
+#First, summarize the original data using grouping_column1 type and planting grouping_column2 as grouping variables.
+
+summarized_stats <- complete_dataset %>%
+  group_by(complete_dataset[,grouping_column1], complete_dataset[,grouping_column2]) %>%
+  summarise(response_column = mean(response_column))
+
 
 #Next, add the group labels as a new variable in the data frame.
+summarized_stats$group <- c("a","b","c","d")
+summarized_stats
 
-mean.yield.data
 
 
-
-two.way.plot <- ggplot(crop.data, aes(x = density, y = yield, group=fertilizer)) +
+two.way.plot <- ggplot(complete_dataset, aes(x = grouping_column2, y = response_column, group=grouping_column1)) +
   geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.1, h = 0))
 
 #Add the means and standard errors to the graph
 two.way.plot <- two.way.plot +
   stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
   stat_summary(fun.data = 'mean_se', geom = 'pointrange') +
-  geom_point(data=mean.yield.data, aes(x=density, y=yield))
+  geom_point(data=summarized_stats, aes(x=grouping_column2, y=response_column))
 
-Split up the data
-
-To show which groups are different from one another, use facet_wrap() to split the data up over the three types of fertilizer. To add labels, use geom_text(), and add the group letters from the mean.yield.data dataframe you made earlier.
+#Split up the data
+#To show which groups are different from one another, use facet_wrap() to split the data up over the three types of grouping_column1. To add labels, use geom_text(), and add the group letters from the summarized_stats dataframe you made earlier.
 
 two.way.plot <- two.way.plot +
-  geom_text(data=mean.yield.data, label=mean.yield.data$group, vjust = -8, size = 5) +
-  facet_wrap(~ fertilizer)
+  geom_text(data=summarized_stats, label=summarized_stats$group, vjust = -8, size = 5) +
+  facet_wrap(~ grouping_column1)
 
 two.way.plot
 
+
+#Make the graph ready for publication
+#In this step we will remove the grey background and add axis labels.
+
+two.way.plot <- two.way.plot +
+  theme_classic2() +
+  labs(title = "response to grouping_column1 grouping_column2",
+      x = "grouping_column2 (1=low grouping_column2, 2=high grouping_column2)",
+      y = "response"
+
+two.way.plot
+}
 
 
