030376e90d6f00a00e1dcf32dd06d801e37f1d10
umontano <u.montano.j@gmail.com>
2022-11-26 18:48:56 -0600

    ggplot one way anova

--- discrete.R
+++ discrete.R
@@ -27,17 +27,13 @@ complete_dataset <-  mtcars
 response_column <- 'mpg'
 grouping_column1 <- 'vs'
 grouping_column2 <- 'am'
+
 two_way_anova_graph <- function(complete_dataset, response_column, grouping_column1, grouping_column2, threshold_significance = 0.05)
 {
+
 #Make a data frame with the group labels
 #Now we need to make an additional data frame so we can add these groupwise differences to our graph.
 #First, summarize the original data using grouping_column1 type and planting grouping_column2 as grouping variables.
-
-ssss <- mtcars %>%
-group_by(am, vs) %>%
-summarize(prom = mean(mpg))
-ssss
-
 
 summarized_stats <- complete_dataset %>%
   group_by(!! as.symbol(grouping_column1), !! as.symbol(grouping_column2)) %>%
@@ -46,14 +42,16 @@ summarized_stats
 
 
 #Next, add the group labels as a new variable in the data frame.
-summarized_stats$group <- c("a","b","c","d")
+#summarized_stats$group <- c("a","b","c","d")
 summarized_stats
 
 
 library(ggplot2)
+library(ggbeeswarm)
 
-two.way.plot <- ggplot(complete_dataset, aes(x = !!as.symbol(grouping_column2), y = !!as.symbol(response_column), group=!!as.symbol(grouping_column1))) +
-  geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.1, h = 0))
+two.way.plot <- ggplot(complete_dataset, aes(get(grouping_column2), get(response_column), group=get(grouping_column1))) +
+  #geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.05, h = 0))
+  geom_beeswarm()
 two.way.plot
 
 #Add the means and standard errors to the graph
@@ -67,7 +65,7 @@ two.way.plot
 #To show which groups are different from one another, use facet_wrap() to split the data up over the three types of grouping_column1. To add labels, use geom_text(), and add the group letters from the summarized_stats dataframe you made earlier.
 
 two.way.plot <- two.way.plot +
-  geom_text(data=summarized_stats, label=summarized_stats$group, vjust = -8, size = 5) +
+#  geom_text(data=summarized_stats, label=summarized_stats$group, vjust = -8, size = 5) +
   facet_wrap(~ get(grouping_column1))
 two.way.plot
 
@@ -76,12 +74,56 @@ two.way.plot
 #In this step we will remove the grey background and add axis labels.
 
 two.way.plot <- two.way.plot +
-  theme_classic2() +
-  labs(title = "response to grouping_column1 grouping_column2",
-      x = "grouping_column2 (1=low grouping_column2, 2=high grouping_column2)",
-      y = "response"
-
+#  theme_classic() +
+  labs(title = paste(grouping_column2, ' Diferrences in ', response_column, '\n Faceted by ', grouping_column1),
+      x = grouping_column2,
+      y = response_column)
 two.way.plot
+
 }
 
+
+
+  
+
+one_way_anova_graph <- function(complete_dataset, response_column, grouping_column1, threshold_significance = 0.05)
+{
+#First, summarize the original data using grouping_column1 type and planting grouping_column2 as grouping variables.
+
+summarized_stats <- complete_dataset %>%
+  group_by(!! as.symbol(grouping_column1)) %>%
+  summarise(mean = mean(!! as.symbol(response_column)))
+summarized_stats
+
+
+#Next, add the group labels as a new variable in the data frame.
+#summarized_stats$group <- c("a","b","c","d")
+summarized_stats
+
+
+library(ggplot2)
+library(ggbeeswarm)
+
+two.way.plot <- ggplot(complete_dataset, aes(get(grouping_column1), get(response_column), col = get(grouping_column1)) +
+  #geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.05, h = 0))
+  geom_beeswarm()
+two.way.plot
+
+#Add the means and standard errors to the graph
+two.way.plot <- two.way.plot +
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange') +
+geom_point(data=summarized_stats, aes(x=get(grouping_column1), y=mean))
+two.way.plot
+
+
+#Make the graph ready for publication
+
+two.way.plot <- two.way.plot +
+#  theme_classic() +
+  labs(title = paste(grouping_column1, '-groups Diferrences in ', response_column),
+      x = grouping_column1,
+      y = response_column)
+two.way.plot
+}
 
