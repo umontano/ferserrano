f27a88117e8f63be567a77354482a059b5e0352a
umontano <u.montano.j@gmail.com>
2022-11-26 20:56:09 -0600

    ggplot one way anova

--- discrete.R
+++ discrete.R
@@ -36,6 +36,8 @@ two_way_anova_graph <- function(complete_dataset, response_column, grouping_colu
 #Make a data frame with the group labels
 #Now we need to make an additional data frame so we can add these groupwise differences to our graph.
 #First, summarize the original data using grouping_column1 type and planting grouping_column2 as grouping variables.
+complete_dataset[,grouping_column1] <- as.factor(complete_dataset[, grouping_column1])
+complete_dataset[,grouping_column2] <- as.factor(complete_dataset[, grouping_column2])
 
 summarized_stats <- complete_dataset %>%
   group_by(!! as.symbol(grouping_column1), !! as.symbol(grouping_column2)) %>%
@@ -45,42 +47,30 @@ summarized_stats
 
 #Next, add the group labels as a new variable in the data frame.
 #summarized_stats$group <- c("a","b","c","d")
+
 summarized_stats
 
 
 library(ggplot2)
 library(ggbeeswarm)
 
-two.way.plot <- ggplot(complete_dataset, aes(get(grouping_column2), get(response_column), group=get(grouping_column1))) +
+two.way.plot <- ggplot(complete_dataset, aes(get(grouping_column2), get(response_column), col = get(grouping_column2), group=get(grouping_column1))) +
   #geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.05, h = 0))
-  geom_beeswarm()
-two.way.plot
-
+  geom_beeswarm() +
 #Add the means and standard errors to the graph
-two.way.plot <- two.way.plot +
   stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
   stat_summary(fun.data = 'mean_se', geom = 'pointrange') +
-geom_point(data=summarized_stats, aes(x=get(grouping_column2), y=mean))
-two.way.plot
-
+geom_point(data=summarized_stats, aes(x=get(grouping_column2), y=mean)) +
 #Split up the data
-#To show which groups are different from one another, use facet_wrap() to split the data up over the three types of grouping_column1. To add labels, use geom_text(), and add the group letters from the summarized_stats dataframe you made earlier.
-
-two.way.plot <- two.way.plot +
 #  geom_text(data=summarized_stats, label=summarized_stats$group, vjust = -8, size = 5) +
-  facet_wrap(~ get(grouping_column1))
-two.way.plot
-
-
+  facet_wrap(~ get(grouping_column1)) +
 #Make the graph ready for publication
-#In this step we will remove the grey background and add axis labels.
-
-two.way.plot <- two.way.plot +
 #  theme_classic() +
   labs(title = paste(grouping_column2, ' Diferrences in ', response_column, '\n Faceted by ', grouping_column1),
       x = grouping_column2,
       y = response_column)
-two.way.plot
+
+return(two.way.plot)
 
 }
 
@@ -90,6 +80,8 @@ two.way.plot
 
 one_way_anova_graph <- function(complete_dataset, response_column, grouping_column1, threshold_significance = 0.05)
 {
+
+complete_dataset[,grouping_column1] <- as.factor(complete_dataset[, grouping_column1])
 #First, summarize the original data using grouping_column1 type and planting grouping_column2 as grouping variables.
 
 summarized_stats <- complete_dataset %>%
@@ -108,25 +100,18 @@ library(ggbeeswarm)
 
 two.way.plot <- ggplot(complete_dataset, aes(get(grouping_column1), get(response_column), col = get(grouping_column1))) +
   #geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.05, h = 0))
-  geom_beeswarm()
-two.way.plot
-
+  geom_beeswarm() +
 #Add the means and standard errors to the graph
-two.way.plot <- two.way.plot +
   stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
   stat_summary(fun.data = 'mean_se', geom = 'pointrange') +
-geom_point(data=summarized_stats, aes(x=get(grouping_column1), y=mean))
-two.way.plot
-
-
+geom_point(data=summarized_stats, aes(x=get(grouping_column1), y=mean)) +
 #Make the graph ready for publication
-
-two.way.plot <- two.way.plot +
 #  theme_classic() +
   labs(title = paste(grouping_column1, '-groups Diferrences in ', response_column),
       x = grouping_column1,
       y = response_column)
-two.way.plot
+
+return(two.way.plot)
 
 }
 
