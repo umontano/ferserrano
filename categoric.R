99788431e93b4e7c6601f82f60ff8d26c5e7c795
umontano <u.montano.j@gmail.com>
2022-11-27 18:01:29 -0600

    added anova_grapsh

--- categoric.R
+++ categoric.R
@@ -104,6 +104,91 @@ anova_graphs_from_lm_pairs_list <- function(complete_dataset, lm_pairs_list, thr
 	return(anova_graphs_list)
 }
 
+
+two_way_anova_graph <- function(complete_dataset, response_column, grouping_column1, grouping_column2, threshold_significance = 0.05)
+{
+
+#Make a data frame with the group labels
+#Now we need to make an additional data frame so we can add these groupwise differences to our graph.
+#First, summarize the original data using grouping_column1 type and planting grouping_column2 as grouping variables.
+complete_dataset[,grouping_column1] <- as.factor(complete_dataset[, grouping_column1])
+complete_dataset[,grouping_column2] <- as.factor(complete_dataset[, grouping_column2])
+
+summarized_stats <- complete_dataset %>%
+  group_by(!! as.symbol(grouping_column1), !! as.symbol(grouping_column2)) %>%
+  summarise(mean = mean(!! as.symbol(response_column)))
+summarized_stats
+
+
+#Next, add the group labels as a new variable in the data frame.
+#summarized_stats$group <- c("a","b","c","d")
+
+summarized_stats
+
+
+library(ggplot2)
+library(ggbeeswarm)
+
+two.way.plot <- ggplot(complete_dataset, aes(get(grouping_column2), get(response_column), col = get(grouping_column2), group=get(grouping_column1))) +
+  #geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.05, h = 0))
+  geom_beeswarm() +
+#Add the means and standard errors to the graph
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange') +
+geom_point(data=summarized_stats, aes(x=get(grouping_column2), y=mean)) +
+#Split up the data
+#  geom_text(data=summarized_stats, label=summarized_stats$group, vjust = -8, size = 5) +
+  facet_wrap(~ get(grouping_column1)) +
+#Make the graph ready for publication
+#  theme_classic() +
+  labs(title = paste(grouping_column2, ' Diferrences in ', response_column, '\n Faceted by ', grouping_column1),
+      x = grouping_column2,
+      y = response_column)
+
+return(two.way.plot)
+
+}
+
+
+
+  
+
+one_way_anova_graph <- function(complete_dataset, response_column, grouping_column1, threshold_significance = 0.05)
+{
+
+complete_dataset[,grouping_column1] <- as.factor(complete_dataset[, grouping_column1])
+#First, summarize the original data using grouping_column1 type and planting grouping_column2 as grouping variables.
+
+summarized_stats <- complete_dataset %>%
+  group_by(!! as.symbol(grouping_column1)) %>%
+  summarise(mean = mean(!! as.symbol(response_column)))
+summarized_stats
+
+
+#Next, add the group labels as a new variable in the data frame.
+#summarized_stats$group <- c("a","b","c","d")
+summarized_stats
+
+
+library(ggplot2)
+library(ggbeeswarm)
+
+two.way.plot <- ggplot(complete_dataset, aes(get(grouping_column1), get(response_column), col = get(grouping_column1))) +
+  #geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.05, h = 0))
+  geom_beeswarm() +
+#Add the means and standard errors to the graph
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange') +
+geom_point(data=summarized_stats, aes(x=get(grouping_column1), y=mean)) +
+#Make the graph ready for publication
+#  theme_classic() +
+  labs(title = paste(grouping_column1, '-groups Diferrences in ', response_column),
+      x = grouping_column1,
+      y = response_column)
+
+return(two.way.plot)
+}
+
 #threshold_significance <- 0.05
 #complete_dataset <- cbind(torrance_percentil, torrance_csv_original)
 #lm_pairs_list <- find_list_significant_differences_in_multi_lm(rrrr)
