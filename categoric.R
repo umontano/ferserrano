fd585c58838f76604a37a75c3422e3c7a7144dc6
umontano <u.montano.j@gmail.com>
2022-11-30 00:51:39 -0600

    renamed and selectedskim _ binwidth optimized

--- categoric.R
+++ categoric.R
@@ -230,7 +230,7 @@ skimmed_dataset <- descriptee_dataset %>%
 	skim %>%
 	#select(-skim_type, -n_missing, -complete_rate, -factor.ordered, -factor.n_unique, -factor.top_counts)
 	select(-skim_type, -n_missing, -complete_rate) %>%
-	rename(variable = skim_type, mean = numeric.mean, sd = numeric.sd, p0 = numeric.p0, p25 = numeric.p25, p50 = numeric.p50, p75 = numeric.p75, p100 = numeric.p100)
+	rename(variable = skim_variable, mean = numeric.mean, sd = numeric.sd, p0 = numeric.p0, p25 = numeric.p25, p50 = numeric.p50, p75 = numeric.p75, p100 = numeric.p100)
 
 
 #lambda definition pipeline
